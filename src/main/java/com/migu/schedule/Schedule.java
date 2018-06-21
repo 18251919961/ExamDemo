9f794927797a1f44ed1be902a13fefcdfb642bf9
cuijian <cuijian70@139.com>
2018-06-21 18:57:46 +0800

    something

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -5,124 +5,98 @@ import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.domain.Task;
 import com.migu.schedule.info.TaskInfo;
 
-import java.awt.print.Pageable;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
 
-    private Set<Integer> allNodes = Collections.synchronizedSet(new TreeSet<Integer>());
-    private Map<Integer, Task> suspendTasks = new ConcurrentHashMap<Integer, Task>();
-    private Map<Integer, Task> executeTasks = new ConcurrentHashMap<Integer, Task>();
+    private Set<Integer> nodes = new TreeSet<Integer>();
+    private Map<Integer, Task> tasks1 = new HashMap<Integer, Task>();
+    private Map<Integer, Task> tasks2 = new HashMap<Integer, Task>();
 
-    private boolean checkInt(int id) {
+    private boolean invalidParam(int id) {
         return id <= 0;
     }
 
-    private boolean existTask(int taskId) {
-        return suspendTasks.containsKey(taskId) || executeTasks.containsKey(taskId);
+    private boolean exist(int id) {
+        return tasks1.containsKey(id) || tasks2.containsKey(id);
     }
 
     private boolean existNode(int nodeId) {
-        return allNodes.contains(nodeId);
+        return nodes.contains(nodeId);
     }
 
     public int init() {
-        allNodes.clear();
-        suspendTasks.clear();
-        executeTasks.clear();
+        nodes.clear();
+        tasks1.clear();
+        tasks2.clear();
         return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
+        if (invalidParam(nodeId)) {
+            return ReturnCodeKeys.E004;
+        }
+        if (existNode(nodeId)) {
+            return ReturnCodeKeys.E005;
+        }
+
+        nodes.add(nodeId);
         return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
+        if (invalidParam(nodeId)) {
+            return ReturnCodeKeys.E004;
+        }
+        if (!existNode(nodeId)) {
+            return ReturnCodeKeys.E007;
+        }
+
+        nodes.remove(nodeId);
         return ReturnCodeKeys.E006;
     }
 
 
     public int addTask(int taskId, int consumption) {
-        if (checkInt(taskId)) return ReturnCodeKeys.E009;
-        if (existTask(taskId)) return ReturnCodeKeys.E010;
+        if (invalidParam(taskId)) {
+            return ReturnCodeKeys.E009;
+        }
+        if (exist(taskId)) {
+            return ReturnCodeKeys.E010;
+        }
 
-        suspendTasks.put(taskId, new Task(taskId, consumption, -1));
+        tasks1.put(taskId, new Task(taskId, consumption, -1));
         return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-        if (checkInt(taskId)) return ReturnCodeKeys.E009;
-        if (!existTask(taskId)) return ReturnCodeKeys.E012;
+        if (invalidParam(taskId)) {
+            return ReturnCodeKeys.E009;
+        }
+        if (!exist(taskId)) {
+            return ReturnCodeKeys.E012;
+        }
 
-        if (suspendTasks.containsKey(taskId)) suspendTasks.remove(taskId);
-        else executeTasks.remove(taskId);
+        if (tasks1.containsKey(taskId)) {
+            tasks1.remove(taskId);
+        }
+        else tasks2.remove(taskId);
 
         return ReturnCodeKeys.E011;
     }
 
 
     public int scheduleTask(int threshold) {
-        if (checkInt(threshold)) return ReturnCodeKeys.E002;
-
-        List<Task> tasks = new ArrayList<Task>(suspendTasks.size() + executeTasks.size());
-
-        if (suspendTasks.size() > 0) {
-            tasks.addAll(suspendTasks.values());
-        } else if (executeTasks.size() <= 0) {
-            return ReturnCodeKeys.E013;
+        if (invalidParam(threshold)) {
+            return ReturnCodeKeys.E002;
         }
 
-        tasks.addAll(executeTasks.values());
-        calcPlan(tasks);
-
-        return ReturnCodeKeys.E000;
-    }
-
-    private void calcPlan(List<Task> tasks) {
-        Deque<Integer> nodes = new LinkedList<Integer>(this.allNodes);
-        Collections.sort(tasks, new Comparator<Task>() {
-            public int compare(Task o1, Task o2) {
-                return o1.getConsumption() - o2.getConsumption();
-            }
-        });
-
-        Map<Integer, List<Integer>> plan = new HashMap<Integer, List<Integer>>();
-        int nodeSize = nodes.size();
-        int taskSize = tasks.size();
-        boolean asc = true;
-        int tempNodeId;
-        List<Integer> tempConsumptions;
-
-        for (int i = 0; i < taskSize; i++) {
-            if (asc) {
-                tempNodeId = nodes.pollFirst();
-            } else {
-                tempNodeId = nodes.pollLast();
-            }
-
-            if (!plan.containsKey(tempNodeId)) {
-                tempConsumptions = new ArrayList<Integer>();
-                plan.put(tempNodeId, tempConsumptions);
-            } else {
-                tempConsumptions = plan.get(tempNodeId);
-            }
-
-            tempConsumptions.add(tasks.get(i).getConsumption());
-
-            if (i % nodeSize == (nodeSize - 1)) {
-                asc = !asc;
-            }
-        }
-    }
-
-    private void execPlan() {
-
+        return ReturnCodeKeys.E013;
     }
 
 
@@ -130,11 +104,11 @@ public class Schedule {
         if (tasks == null) return ReturnCodeKeys.E016;
 
         tasks.clear();
-        for (Task task : suspendTasks.values()) {
+        for (Task task : tasks1.values()) {
             tasks.add(covertTask2TaskInfo(task));
         }
 
-        for (Task task : executeTasks.values()) {
+        for (Task task : tasks2.values()) {
             tasks.add(covertTask2TaskInfo(task));
         }
 
